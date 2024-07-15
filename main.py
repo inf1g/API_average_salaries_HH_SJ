5a6c21a55eb6f66fd01cc4e02d81bd43d4d3244d
inf <manskiy2k@gmail.com>
2024-07-15 20:24:47 +0300

    fix 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from requests import Session
+import requests
 import os
+from time import sleep
 from itertools import count
 from dotenv import load_dotenv
 from terminaltables import AsciiTable
@@ -11,8 +12,8 @@ def load_key(token):
     return key
 
 
-def get_response(session, url, headers=None, params=None):
-    response = session.get(url, headers=headers, params=params)
+def get_response(url, headers=None, params=None):
+    response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
     return response.json()
 
@@ -35,14 +36,11 @@ def calculate_salary(vacancy, salary_from, salary_to, toggle=None):
         vacancy = vacancy['salary']
     if vacancy:
         if vacancy.get(salary_from) and vacancy.get(salary_to):
-            if vacancy.get(salary_from) != 0 and vacancy.get(salary_to) != 0:
-                salary.append(int((vacancy.get(salary_from) + vacancy.get(salary_to)) / 2))
+            salary.append(int((vacancy.get(salary_from) + vacancy.get(salary_to)) / 2))
         elif vacancy.get(salary_from) and not vacancy.get(salary_to):
-            if vacancy.get(salary_from) != 0 and vacancy.get(salary_to) == 0:
-                salary.append(int(vacancy.get(salary_from) * 1.2))
+            salary.append(int(vacancy.get(salary_from) * 1.2))
         elif vacancy.get(salary_to) and not vacancy.get(salary_from):
-            if vacancy.get(salary_to) != 0 and vacancy.get(salary_from) == 0:
-                salary.append(int(vacancy.get(salary_to) * 0.8))
+            salary.append(int(vacancy.get(salary_to) * 0.8))
     return salary
 
 
@@ -55,41 +53,34 @@ def predict_rub_salary_for_hh(languages):
     period_days = 30
     per_page_vacancies = 100
     currency = 'RUR'
-    with Session() as session:
-        for language in languages:
-            print(language)
-            vacancies_processed_total = 0
-            for page in count(0):
-                headers = {
-                    'HH-User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-                                     ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
-                }
-                params = {
-                    'text': f'Программист {language}',
-                    'area': area_moscow,
-                    'page': page,
-                    'per_page': per_page_vacancies,
-                    'period': period_days,
-                    'currency': currency
-                }
-                vacancies_response = get_response(session, url, headers, params)
-                for vacancy in vacancies_response['items']:
-                    vacancy_salary = calculate_salary(vacancy, 'from', 'to', 1)
-                    salaries_vacancies.extend(vacancy_salary)
-                    vacancies_processed_total += len(vacancy_salary)
-                if page >= vacancies_response['pages'] - 1:
-                    break
-            if len(salaries_vacancies) == 0:
-                average_salary = 0
-            else:
-                average_salary = int(sum(salaries_vacancies) / len(salaries_vacancies))
-            print(f"{vacancies_response['found']} found")
-            print(f"{vacancies_response['pages']} pages")
-            lang = {
-                "vacancies_found": vacancies_response['found'],
-                "vacancies_processed": vacancies_processed_total,
-                "average_salary": int(average_salary)}
-            vacancy_statistics[language] = lang
+    for language in languages:
+        vacancies_processed_total = 0
+        sleep(10)
+        for page in count(0):
+            params = {
+                'text': f'Программист {language}',
+                'area': area_moscow,
+                'page': page,
+                'per_page': per_page_vacancies,
+                'period': period_days,
+                'currency': currency
+            }
+            vacancies_response = get_response(url, params=params)
+            for vacancy in vacancies_response['items']:
+                vacancy_salary = calculate_salary(vacancy, 'from', 'to', 1)
+                salaries_vacancies.extend(vacancy_salary)
+                vacancies_processed_total += len(vacancy_salary)
+            if page >= vacancies_response['pages'] - 1:
+                break
+        if len(salaries_vacancies) == 0:
+            average_salary = 0
+        else:
+            average_salary = int(sum(salaries_vacancies) / len(salaries_vacancies))
+        lang = {
+            "vacancies_found": vacancies_response['found'],
+            "vacancies_processed": vacancies_processed_total,
+            "average_salary": int(average_salary)}
+        vacancy_statistics[language] = lang
     return vacancy_statistics
 
 
@@ -99,36 +90,36 @@ def predict_rub_salary_for_sj(languages, api_key):
     area_moscow = 4
     per_page_vacancies = 100
     currency = 'rub'
-    with Session() as session:
-        for language in languages:
-            salaries_vacancies = []
-            vacancies_processed_total = 0
-            for page in count(0):
-                headers = {'X-Api-App-Id': api_key}
-                params = {
-                    "keyword": f'Программист {language}',
-                    'town': area_moscow,
-                    'page': page,
-                    'count': per_page_vacancies,
-                    'currency': currency
-                }
-                vacancies_response = get_response(session, url, headers, params)
-                for vacancy in vacancies_response['objects']:
-                    vacancy_salary = calculate_salary(vacancy, 'payment_from', 'payment_to')
-                    salaries_vacancies.extend(vacancy_salary)
-                    vacancies_processed_total += len(vacancy_salary)
-                if not vacancies_response['more']:
-                    break
-            if len(salaries_vacancies) == 0:
-                average_salary = 0
-            else:
-                average_salary = int(sum(salaries_vacancies) / len(salaries_vacancies))
-            lang = {
-                "vacancies_found": vacancies_response['total'],
-                "vacancies_processed": vacancies_processed_total,
-                "average_salary": int(average_salary)
+    for language in languages:
+        salaries_vacancies = []
+        vacancies_response = 0
+        vacancies_processed_total = 0
+        for page in count(0):
+            headers = {'X-Api-App-Id': api_key}
+            params = {
+                "keyword": f'Программист {language}',
+                'town': area_moscow,
+                'page': page,
+                'count': per_page_vacancies,
+                'currency': currency
             }
-            vacancy_statistics[language] = lang
+            vacancies_response = get_response(url, headers, params)
+            for vacancy in vacancies_response['objects']:
+                vacancy_salary = calculate_salary(vacancy, 'payment_from', 'payment_to')
+                salaries_vacancies.extend(vacancy_salary)
+                vacancies_processed_total += len(vacancy_salary)
+            if not vacancies_response['more']:
+                break
+        if len(salaries_vacancies) == 0:
+            average_salary = 0
+        else:
+            average_salary = int(sum(salaries_vacancies) / len(salaries_vacancies))
+        lang = {
+            "vacancies_found": vacancies_response['total'],
+            "vacancies_processed": vacancies_processed_total,
+            "average_salary": int(average_salary)
+        }
+        vacancy_statistics[language] = lang
     return vacancy_statistics
 
 
@@ -145,7 +136,6 @@ def main():
         "Go",
         "C",
         "KOTLIN",
-        'Ruby',
         "1C"
     }
     statistic_sj = predict_rub_salary_for_sj(languages, key)
